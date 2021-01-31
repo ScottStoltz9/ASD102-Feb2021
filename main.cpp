74b8b8581689b8979acd9b06ae7c91b1fc4fe25a
Quentin Miller <72428419+asdqmiller@users.noreply.github.com>
2021-01-30 23:29:20 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//This is a sample to be used only for education purposes.
+//
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello and Welcome to ASD102!" << endl;
+  
+ return 0;
+}
