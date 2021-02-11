237ed811378f8f277a5ad1b23c8337cffb93c943
Quentin Miller <72428419+asdqmiller@users.noreply.github.com>
2021-02-10 19:13:45 -0800

    Add files via upload

---
+++ ExampleCh2_Syntax_Errors.cpp
@@ -0,0 +1,16 @@
+#include<iotream> 
+ 
+using namespace std; 
+ 
+int main() 
+{ 
+    int num
+
+    num = 18; 
+
+    tempNum = 2 * num;
+
+    cout >> "Num = " << num << ", Tempnum = " < tempNum << endl; 
+
+    return 0;
+}
